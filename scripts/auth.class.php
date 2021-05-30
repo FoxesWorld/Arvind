8c9115b9b8118822a99d4feb2c975d77eda2c4f7
Miomoor <45480987+miomoor@users.noreply.github.com>
2021-05-30 20:18:20 +0300

    auth.class.php 0.0.2.5 Session Fix

--- scripts/auth.class.php
+++ scripts/auth.class.php
@@ -11,12 +11,16 @@
 -----------------------------------------------------
  File: auth.class.php
 -----------------------------------------------------
- Version: 0.0.2.2 Experimental
+ Version: 0.0.2.5 Experimental
 -----------------------------------------------------
  Usage: Auth + SetSession + LoadFiles
 =====================================================
 */
 
+/* TODO 
+ * Merge usersession into fox_launcher
+ * Merge sip into fox_launcher
+ */
 	class auth {
 		
 		protected $action;
@@ -24,6 +28,7 @@
 		protected $inputUser;
 		protected $inputPass;
 		protected $ctoken;
+		private $db;
 		
 		/* RealData */
 		protected $realPass;
@@ -35,12 +40,15 @@
 		protected $checkPass;
 		protected $launchermd5;
 		
-		function __construct ($action, $client, $login, $postPass, $launchermd5, $ctoken){
+		private $debug = false;
+		
+		function __construct ($action, $client, $login, $postPass, $launchermd5, $ctoken, $db){
 			require_once (SCRIPTS_DIR.'loadFiles.class.php');
 			require_once (SCRIPTS_DIR.'geoIP.class.php');
-			global $db, $config;
+			global $config;
 					try {
 						
+						$this->db 			= $db;
 						$this->launchermd5  = $this->pregMatch($launchermd5);
 						$this->action 		= $this->pregMatch($action);
 						$this->inputUser 	= $this->pregMatch($login);
@@ -59,11 +67,11 @@
 							}
 
 							$this->sessID = token();
-							$stmt = $db->prepare("SELECT user, token FROM usersession WHERE user= :login");
+							$stmt = $this->db->prepare("SELECT user, token FROM usersession WHERE user= :login");
 							$stmt->bindValue(':login', $this->inputUser);
 							$stmt->execute();
 							$rU = $stmt->fetch(PDO::FETCH_ASSOC);
-							if($rU['user'] != null) {
+							if(is_array($rU) && $rU['user'] != null) {
 								$this->realUser = $rU['user'];
 							}
 
@@ -74,14 +82,16 @@
 							}
 							$this->setSession();
 
-							$db->run("UPDATE LOW_PRIORITY ".$config['db_table']." SET lastdate='".CURRENT_TIME."', logged_ip='".REMOTE_IP."' WHERE name='$this->inputUser'");
+							$this->db->run("UPDATE LOW_PRIORITY ".$config['db_table']." SET lastdate='".CURRENT_TIME."', logged_ip='".REMOTE_IP."' WHERE name='$this->inputUser'");
 							if($this->action == 'auth') {
 								$geoplugin = new geoPlugin();
 								$loadFiles = new loadFiles($client, $this->sessID, $this->accessToken, $this->realUser);
 							}
+						} else {
+							die(JSONanswer('type', 'error', 'message', 'Launcher was modifficated!'));
 						}
 			} catch(PDOException $pe) {
-				die(Security::encrypt("errorsql<$>", $config['key1']).$pe);
+				die(Security::encrypt("errorsql<$>".$pe, $config['key1']).$pe);
 			} 
 		}
 		
@@ -94,23 +104,23 @@
 			return $toCheck;
 		}
 		
-		private function antiBrute(){
-			global $config, $db;
+		private function antiBrute() {
+			global $config;
 				if($config['useantibrut'] === true) {
-					$stmt = $db->getRow("SELECT sip,time FROM sip WHERE sip='".REMOTE_IP."' And time >'".CURRENT_TIME."'");
+					$stmt = $this->db->getRow("SELECT sip,time FROM sip WHERE sip='".REMOTE_IP."' And time >'".CURRENT_TIME."'");
 					$bannedIP = $stmt['sip'];
 					if(REMOTE_IP == $bannedIP) {
-						$stmt = $db->run("DELETE FROM sip WHERE time < '".CURRENT_TIME."';");
+						$stmt = $this->db->run("DELETE FROM sip WHERE time < '".CURRENT_TIME."';");
 						exit(Security::encrypt("temp<$>", $config['key1']));
 					}
 						
 					if ($this->inputUser != $this->realUser) {
-						$stmt = $db->run("INSERT INTO sip (sip, time)VALUES ('".REMOTE_IP."', '".$config['bantime']."')");
+						$stmt = $this->db->run("INSERT INTO sip (sip, time)VALUES ('".REMOTE_IP."', '".$config['bantime']."')");
 						exit(Security::encrypt("errorLogin<$>", $config['key1']));
 					}
 					
 					if(!strcmp($this->realPass,$this->checkPass) == 0 || !$this->realPass) {
-						$stmt = $db->run("INSERT INTO sip (sip, time)VALUES ('".REMOTE_IP."', '".$config['bantime']."')");
+						$stmt = $this->db->run("INSERT INTO sip (sip, time)VALUES ('".REMOTE_IP."', '".$config['bantime']."')");
 						exit(Security::encrypt("errorLogin<$>", $config['key1']));
 					}
 
@@ -122,21 +132,24 @@
 		}
 		
 		private function setSession() {
-			global $db;
-			if($this->inputUser == $this->realUser) {
+			global $config;
+
+			if($this->inputUser == $this->realUser && $this->ctoken != "null") {
 				if($this->ctoken == "null") {
-					$stmt = $db->prepare("UPDATE usersession SET session = '".$this->sessID."', token = :token WHERE user = :login");
+					exit(Security::encrypt("Setting a Session", $config['key1']));
+					$stmt = $this->db->prepare("UPDATE usersession SET session = '".$this->sessID."', token = :token WHERE user = :login");
 					$stmt->bindValue(':token', $this->accessToken);
 				} else {
-					$stmt = $db->prepare("UPDATE usersession SET session = '".$this->sessID."' WHERE user = :login");
+					$stmt = $this->db->prepare("UPDATE usersession SET session = '".$this->sessID."' WHERE user = :login");
 				}
 
 				$stmt->bindValue(':login', $this->realUser);
 				$stmt->execute();
 
 			} else {
-				if($this->ctoken == "null" || $this->inputUser != $this->realUser) {
-					$stmt = $db->prepare("INSERT INTO usersession (user, session, md5, token) VALUES (:login, '".$this->sessID."', :md5, '".$this->accessToken."')");
+
+				if($this->checkUserSession() !== null || $this->inputUser != $this->realUser) {
+					$stmt = $this->db->prepare("INSERT INTO usersession (user, session, md5, token) VALUES (:login, '".$this->sessID."', :md5, '".$this->accessToken."')");
 					$stmt->bindValue(':login', $this->realUser);
 					$stmt->bindValue(':md5', str_replace('-', '', uuidConvert($this->realUser)));
 					$stmt->execute();
@@ -145,8 +158,8 @@
 		}
 		
 		private function selectRealData($login) {
-			global $db, $config;
-				$stmt = $db->prepare("SELECT ".$config['db_columnUser'].",".$config['db_columnPass']." FROM ".$config['db_table']." WHERE BINARY ".$config['db_columnUser']." = :login");
+			global $config;
+				$stmt = $this->db->prepare("SELECT ".$config['db_columnUser'].",".$config['db_columnPass']." FROM ".$config['db_table']." WHERE BINARY ".$config['db_columnUser']." = :login");
 				$stmt->bindValue(':login', $login);
 				$stmt->execute();
 				$stmt->bindColumn($config['db_columnPass'], $this->realPass);
@@ -158,5 +171,19 @@
 				'Pass: <b>'. $this->realPass.'</b>';
 				}
 				$stmt->fetch();	
+		}
+		
+		private function checkUserSession(){
+				$userSession = '';
+				$stmt = $this->db->prepare("SELECT * FROM usersession WHERE user = :login");
+				$stmt->bindValue(':login', $this->realUser);
+				$stmt->bindColumn('name', $userSession);
+				$stmt->fetch();
+
+					if($userSession == null || $userSession == ''){
+						return null;
+					} else {
+						return $userSession;
+					}
 		}
 	}
